df7d1342282bf2d1c47c46a76925dd3775f15784
belubeluga <belugotz@gmail.com>
2024-08-29 16:26:56 -0300

    updatessss:)))

--- f1.py
+++ f1.py
@@ -15,6 +15,7 @@ def f1(x):
 # plt.show() 
 
 """ EVALUANDO EN LOS PUNTOS QUE USAMOS PARA ARMAR EL LAGRANGE """
+""" ( LAGRANGE EQUIESPACIADO ) """
 erroresEnDataset = [] #errores propios de trabajar con floats
 
 for i in range(1, 50):
@@ -93,18 +94,27 @@ media_de_error cantidad_de_puntos_equisespaciados
 
 
 
-""" primer observacion: ELECCIÓN DE CANTIDAD DE DATOS/ LINESPACE PARA LAGRANGE
+""" primer observacion: ELECCIÓN DE CANTIDAD DE DATOS
+
+( LAGRANGE EQUIESPACIADO )
+
 RUNGE: mientras mas iteraciones, menos precision con lagrange
 (de 100 a 1000)
 [1.13797860e-15 1.01432751e-11 1.35347009e-08 6.36585596e-05
  4.08150754e-02 1.53964245e+02 1.20567439e+06 1.83115839e+09
- 8.21370479e+12] """
+ 8.21370479e+12] 
+ 
+evaluar LINESPACE PARA LAGRANGE
+-> al ser equiespaciado el linespace va a ser parejo en todo el dominio => va a depender de la cantidad de puntos """
 
 
-
+""" evaluar LINESPACE PARA LAGRANGE
+-> 
+"""
 
 
 """
+------> comparaciones a hacer:
 lagrangeInterpol = scipy.lagrange(x, y)
 csInterpol = scipy.CubicSpline(x, y)
 
