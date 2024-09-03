fcd6404fdf037499d27c7de8dc61e7ff072262a4
belubeluga <belugotz@gmail.com>
2024-09-03 00:03:00 -0300

    pruebas con f2 equiespaciados y chebyshev (lineal y cubica)

---
+++ f2evaluacionEquiespaciados.py
@@ -0,0 +1,77 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import griddata
+from mpl_toolkits.mplot3d import Axes3D
+
+def f2(x1, x2):
+    term1 = 0.75 * np.exp(-((9 * x1 - 2)**2 / 4) - ((9 * x2 - 2)**2 / 4))
+    term2 = 0.75 * np.exp(-((9 * x1 + 1)**2 / 49) - ((9 * x2 + 1)**2 / 10))
+    term3 = 0.5 * np.exp(-((9 * x1 - 7)**2 / 4) - ((9 * x2 - 3)**2 / 4))
+    term4 = -0.2 * np.exp(-((9 * x1 - 7)**2 / 4) - ((9 * x2 - 3)**2 / 4))
+    
+    return term1 + term2 + term3 + term4
+
+# Puntos equiespaciados
+n_points = 20
+x_eq = np.linspace(-1, 1, n_points)
+y_eq = np.linspace(-1, 1, n_points)
+x1_eq, x2_eq = np.meshgrid(x_eq, y_eq)
+z_eq = f2(x1_eq, x2_eq)
+
+# Puntos de evaluación INTERMEDIOS (que no coincidan con los NODOS)
+puntosIntermedios = np.linspace(-1, 1, 500)
+x1_inter, x2_inter = np.meshgrid(puntosIntermedios, puntosIntermedios)
+z_inter = f2(x1_inter, x2_inter)
+
+# Interpolaciones
+points = np.array([x1_eq.flatten(), x2_eq.flatten()]).T
+values = z_eq.flatten()
+grid_x, grid_y = np.mgrid[-1:1:500j, -1:1:500j]
+
+# lineal
+z_linear_interpol = griddata(points, values, (grid_x, grid_y), method='linear')
+
+# cúbica
+z_cubic_interpol = griddata(points, values, (grid_x, grid_y), method='cubic')
+
+""" GRÁFICOS 3D """
+fig = plt.figure(figsize=(18, 6))
+
+# f2
+ax1 = fig.add_subplot(131, projection='3d')
+ax1.plot_surface(x1_inter, x2_inter, z_inter, cmap='viridis')
+ax1.set_title('Función Original')
+
+# lineal
+ax2 = fig.add_subplot(132, projection='3d')
+ax2.plot_surface(grid_x, grid_y, z_linear_interpol, cmap='viridis')
+ax2.set_title('Interpolación Lineal')
+
+# cúbica
+ax3 = fig.add_subplot(133, projection='3d')
+ax3.plot_surface(grid_x, grid_y, z_cubic_interpol, cmap='viridis')
+ax3.set_title('Interpolación Cúbica')
+
+plt.tight_layout()
+plt.show()
+
+""" GRÁFICOS """
+plt.figure(figsize=(18, 6))
+
+plt.subplot(1, 3, 1) # Función original
+plt.contourf(x1_inter, x2_inter, z_inter, cmap='viridis')
+plt.title('Función Original')
+plt.colorbar()
+
+plt.subplot(1, 3, 2) # Interpolación lineal
+plt.contourf(grid_x, grid_y, z_linear_interpol, cmap='viridis')
+plt.title('Interpolación Lineal')
+plt.colorbar()
+
+plt.subplot(1, 3, 3) # Interpolación cúbica
+plt.contourf(grid_x, grid_y, z_cubic_interpol, cmap='viridis')
+plt.title('Interpolación Cúbica')
+plt.colorbar()
+
+plt.tight_layout()
+plt.show()
